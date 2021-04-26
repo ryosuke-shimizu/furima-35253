ef15a9ef11a1bd1626cc9b2f9083c62808fc752e
ryosuke-shimizu <v7aclavirex@gmail.com>
2021-04-26 16:43:49 +0900

    エラーメッセージの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
-  has_many :buyers
+  # has_many :buyers
 
-  with_options presence: true do
-    validates :nickname, length: { maximum: 40 }
-    validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/}
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :birthday
-  end
+  #with_options presence: true do
+    #validates :nickname
+    #validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/}
+    #validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    #validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    #validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    #validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    #validates :birthday
+  #end
 end
