b85ae549fbe9916a4b959de211b9c0d53ad76de5
ryosuke-shimizu <v7aclavirex@gmail.com>
2021-04-29 23:06:07 +0900

    itemテーブル作成

--- db/migrate/20210421023546_create_items.rb
+++ db/migrate/20210421023546_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :product_name
+      t.references :user, foreign_key: true
+      t.integer :category_id
+      t.integer :status_id
+      t.integer :shipp_cost_id
+      t.integer :prefecture_id
+      t.integer :shipp_date_id
+      t.integer :price
+      t.text :description
       t.timestamps
     end
   end
