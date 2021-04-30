42833ad3769bebb8326a22d3e2e4b6aea9067ea8
ryosuke-shimizu <v7aclavirex@gmail.com>
2021-04-30 15:37:12 +0900

    createアクション、Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -66,3 +66,6 @@ gem 'sprockets'
 gem 'devise'
 
 gem 'pry-rails'
+
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  #def new
+    #@item = Item.new
+  #end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :product_name, :user, :category_id, :status_id, :shipp_cost_id, :prefecture_id, :shipp_date_id, :price, :description)
+  end
 end
