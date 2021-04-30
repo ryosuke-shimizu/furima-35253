a3a44d741f6a663cf2a042ab84dfa19160b291ea
ryosuke-shimizu <v7aclavirex@gmail.com>
2021-05-01 00:15:13 +0900

    ActiveHash導入

---
+++ app/models/category.rb
@@ -0,0 +1,17 @@
+class Category < ActiveHash::Base
+  include ActiveHash::Associations
+  belongs_to :item
+
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'レディース' },
+    { id: 3, name: 'メンズ' },
+    { id: 4, name: 'ベビー・キッズ' },
+    { id: 5, name: 'インテリア・住まい・小物' },
+    { id: 6, name: '本・音楽・ゲーム' },
+    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
+    { id: 8, name: 'スポーツ・レジャー' },
+    { id: 9, name: 'ハンドメイド' },
+    { id: 10, name: 'その他' }
+  ]
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :salesstatus
+  belongs_to_active_hash :shippingfeestatus
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :scheduleddelivery
   belongs_to :user
   #has_one :buyer
   has_one_attached :image
